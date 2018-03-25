f2a23b28fe6aba4389bf25e99c0d9b3415c3dc04
Waweru <developer.waweru@gmail.com>
2018-03-25 13:12:17 +0300

    Creating the website's menu

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+    $("#navigation").hide();
+}
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+    $("#navigation").show();
+}
