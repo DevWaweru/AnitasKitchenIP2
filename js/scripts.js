e7b8de2ba543c957c741846e1708eb24185fed4d
Waweru <developer.waweru@gmail.com>
2018-03-25 22:43:09 +0300

    Google Analytics added

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,10 @@ function closeNav(){
     document.getElementById("myNav").style.width = "0%";
     $("#navigation").show();
 }
+$(document).ready(function(){
+    $(".row").waypoint(function(){
+        $(this).fadeIn(750);
+    },{
+        offset:'bottom-in-view'
+    })
+})
