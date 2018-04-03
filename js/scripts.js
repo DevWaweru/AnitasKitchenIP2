1787df9484cf0427349648d8c823b53eec8b087b
Waweru <developer.waweru@gmail.com>
2018-04-03 17:40:01 +0300

    Editing the Mailchimp API

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-$(document).ready(function(){
-    $(".fa-bars").click(function(){
-        document.getElementById("big-menu").style.width = "100%"; 
+$(document).ready(function () {
+    $(".fa-bars").click(function () {
+        document.getElementById("big-menu").style.width = "100%";
         $("#navigation").hide();
     });
-    $(".closebtn").click(function(){
+    $(".closebtn").click(function () {
         document.getElementById("big-menu").style.width = "0%";
-        $("#navigation").show();        
+        $("#navigation").show();
     });
+
+    (function ($) {
+        window.fnames = new Array();
+        window.ftypes = new Array();
+        fnames[0] = 'EMAIL';
+        ftypes[0] = 'email';
+    }(jQuery));
+    var $mcj = jQuery.noConflict(true);
 });
